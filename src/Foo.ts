b5ea673a80a9584c0a58e035bb567de4fbaf962f
Zhen She <neal.seah@gmail.com>
2020-10-27 16:38:18 +0800

    test: add acceptance test

--- src/Foo.ts
+++
@@ -1,5 +0,0 @@
-export class Foo {
-  bar() {
-    return 'bar'
-  }
-}
--- test/Foo.test.ts
+++
@@ -1,7 +0,0 @@
-import {Foo} from '../src/Foo'
-
-describe('Foo', () => {
-  it('bar', () => {
-    expect(new Foo().bar()).toEqual('bar')
-  })
-})
